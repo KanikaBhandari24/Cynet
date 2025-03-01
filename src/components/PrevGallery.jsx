f0a728570ae9279bbf7725ff3a222e765aaf3274
Yasir-prog <ya202486@gmail.com>
2025-03-01 18:10:01 +0530

    Updated

--- src/components/PrevGallery.jsx
+++ src/components/PrevGallery.jsx
@@ -13,8 +13,8 @@ function PrevGallery(){
     return(
         <div className="bg-[#DAFCE0]">
             <h1 className="text-[88px]/[89px] tracking-tighter text-[rgb(29,57,43)] mont-bold ml-[156px] py-[75px] w-[50vw] font-bold">Glimpse of Previous Cynet</h1>
-            <div className=" py-14">
-                    <div className="flex list-none flex-wrap gap-x-6 gap-y-4 mx-[156px] h-64 overflow-y-scroll">
+            <div className=" py-12">
+                    <div className="flex list-none flex-wrap gap-x-6 gap-y-4 mx-[156px]">
                         {images.map((image, index)=><li className="h-[25vh] grow-[1]" key={index}><img className="rounded-2xl max-h-full min-w-full block object-cover object-center" src={image} alt={`Previous Cynet Image ${index}`} key={index} /></li>)}
                     </div>
             </div>
